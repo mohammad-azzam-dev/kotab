918fedbed7bb737770d5e59b593834589a0c0223
mohammad azzam <mohammad.azzam.developer@gmail.com>
2023-02-28 20:38:32 +0200

    first commit

--- public/assets/backend/js/script.js
+++
@@ -1,7 +0,0 @@
-function toggle_sidebar() {
-  var element = document.getElementById("sidebar");
-  element.classList.toggle("active");
-
-  var element = document.getElementById("main-panel");
-  element.classList.toggle("active");
-}
--- public/assets/frontend/js/script.js
+++
@@ -1,4 +0,0 @@
-function toggle_sidebar() {
-    var element = document.getElementById("nav-list");
-    element.classList.toggle("active");
-  }
